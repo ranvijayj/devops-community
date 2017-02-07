d7b698b29b520da7028718a171fa3d4773eddf86
ranvijayj <ranvijayj@gmail.com>
2017-02-07 16:11:28 +0530

    Update scaleup.sh

--- elasticsearch-scale/scaleup.sh
+++ elasticsearch-scale/scaleup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-
+##################Change ELB name#####################
 NAME="elb-name"
-
 AZ="ap-south-1a"
 date_end=`date -u +"%Y-%m-%dT%H:%M:%SZ"`
 #date --date "$date_end -10 min"
@@ -10,7 +9,7 @@ date_start=`date -u +"%Y-%m-%dT%H:%M:%SZ" --date "$date_end -5 min"`
 original_cluster_nodes_count=$1
 
 
-#######################################################ELB STEPS###########################################################
+##################ELB STEPS###########################################################
 
 
 var=`/usr/local/bin/aws cloudwatch get-metric-statistics --namespace AWS/ELB --metric-name  Latency --start-time $date_start  --end-time $date_end  --period 60   --statistics Average --dimensions '[{"Name":"LoadBalancerName","Value":"'$NAME'"},{"Name":"AvailabilityZone","Value":"'$AZ'"}]' --unit Seconds | awk '{print $2}' `
@@ -21,23 +20,19 @@ var2=$(echo "scale=2; ($total/5.0) *100 "|bc)
 
 delay=${var2%.*}
 
-#500 below means 5 seconds################
+#################500 below means 5 seconds################
 
 function scale_up ()
 {
 if [ $delay -ge 300 ]; then
 
-###If condition is true, then increase node count by 2
-
+#################count increases by 2#####################
         count=2
+#####################Replace name by your AWS ES domain name, not URL##################
         /usr/local/bin/aws es describe-elasticsearch-domain-config --domain-name name > test123.txt
         instance_current_count=`cat test123.txt | awk '{print $3}'| awk 'NR==7'`
         instance_new_auto_count=`expr $count + $instance_current_count`
-
-###Update cluster configuration#############
-
         /usr/local/bin/aws es update-elasticsearch-domain-config --elasticsearch-cluster-config "InstanceType=m4.large.elasticsearch,InstanceCount=$instance_new_auto_count" --domain-name name
 fi
 }
 scale_up
-
